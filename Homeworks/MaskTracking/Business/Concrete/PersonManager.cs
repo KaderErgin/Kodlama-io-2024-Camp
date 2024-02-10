4ebb5bffc60f277d99cda9f9d2b55d013ebf2866
Kader Ergin <kader.ergin.37@gmail.com>
2024-02-10 18:16:40 +0300

    Add files via upload

---
+++ Homeworks/MaskTracking/Business/Concrete/PersonManager.cs
@@ -0,0 +1,26 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using MernisServiceReference;
+
+namespace Business.Concrete
+{
+    public class PersonManager : IApplicantService
+    {
+        // Encapsulation
+        public void ApplyForMask(Person person)
+        {
+        }
+
+        public List<Person> GetList()
+        {
+            return null;
+        }
+
+        public bool CheckPerson(Person person)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            Task<TCKimlikNoDogrulaResponse> tcKimlikTask = client.TCKimlikNoDogrulaAsync(person.NationalIdentity,person.FirstName,person.LastName,person.DateOfBirthYear);
+            return tcKimlikTask.Result.Body.TCKimlikNoDogrulaResult;
+        }
+    }
+}
